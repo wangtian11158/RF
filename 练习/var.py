0703da9b2f4bc0acdc0b6995b09ba3743071a436
wang.tian <Wang.Tian@elandsystems.com.cn>
2016-11-07 19:59:55 +0800

    zhushi

---
+++ 练习/var.py
@@ -0,0 +1,8 @@
+# -*- coding:cp936 -*-
+import random
+__all__ =['testurl','searchText','LIST_vars','integer']
+
+testurl='http://www.baidu.com'
+searchText=u'文件形式搜索'
+LIST_vars=['hello','list','word']
+integer=random.randint(1,10)
